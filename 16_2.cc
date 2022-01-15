c9a48b8a6d14d9ba592dc1a876f4aa17173ab034
Akrat Chandola <akratchandola@gmail.com>
2022-01-15 10:23:06 +0530

    Add files via upload

---
+++ 16_2.cc
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+void swap(int *x, int *y)
+{
+    int temp=*x;
+   	*x=*y;
+   	*y=temp;
+}
+void sort(int arr[] , int n)
+{
+	int idx;
+    for(int i=0;i<n-1;i++)
+    {
+    	idx=i;
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j] <arr[i])
+            	idx=j;
+            swap(arr[idx],arr[i]);
+        }
+    }
+}
+int main()
+{
+    int A[] ={10,9,8,7,6};
+    int n = 5;
+    sort(A,n);
+    for(int i=0;i<n;i++)
+        cout<<A[i]<<" ";
+    return 0;
+}
